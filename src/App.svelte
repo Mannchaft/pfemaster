d71a05efd87a9272efca08de4ad77bb4f7bc1896
unknown <2kick2014@gmail.com>
2022-05-04 22:15:27 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let name: string;
+  let a: Boolean;
+  a = false;
+  function click() {
+    if (!a) {
+      name = "Worldi";
+    } else {
+      name = "World";
+    }
+    a = !a;
+  }
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <button class="button-29" on:click={click}>Slight change</button>
+</main>
+
+<style type="text/scss">
+  @import "./styles/vars";
+  @import "./styles/classes";
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
